5018600b055d34bd734adce7e37fba26d5a0ffb1
CAT-i0n <73797488+CAT-i0n@users.noreply.github.com>
2022-05-09 17:47:14 +0300

    Rewrite controls with commnad pattern

--- PPVIS_lab3/player.py
+++ PPVIS_lab3/player.py
@@ -1,21 +1,72 @@
 import pygame
 from math import pi, cos, sin
 from time import time
+from abc import ABC
+
+class ButtonPressCommand(ABC):
+    def execute(self, ship):
+        pass
+
+class ButtonPressReturnCommand(ButtonPressCommand):
+    def execute(self, ship):
+        if time() - ship.last_shot > ship.shot_delay:
+            if not pygame.mixer.Channel(1).get_busy():
+                pygame.mixer.Channel(1).set_volume(0.05)
+                pygame.mixer.Channel(1).play(
+                pygame.mixer.Sound(r'sounds\fire.mp3'))
+            ship.shots.append(Shot(ship))
+            ship.last_shot = time()
+            if ship.side == 1:
+                ship.side = -1
+            elif ship.side == -1:
+                ship.side = 1
+
+class ButtonPressWCommand(ButtonPressCommand):
+    def execute(self, ship):
+        ship.speed_x += cos((ship.rot % 360) * pi / 180 + pi / 2) / 10
+        ship.speed_y += -1*sin((ship.rot % 360) * pi / 180 + pi / 2) / 10
+        if (ship.speed_x ** 2 + ship.speed_y ** 2) ** 0.5 > ship.speed_limit:
+            ship.speed_x *= ship.speed_limit / (ship.speed_x ** 2 + ship.speed_y ** 2)**0.5
+            ship.speed_y *= ship.speed_limit / (ship.speed_x ** 2 + ship.speed_y ** 2)**0.5
+        ship.is_move = True
+        
+class ButtonPressACommand(ButtonPressCommand):
+    def execute(self, ship):
+        ship.rot += 5
+
+class ButtonPressDCommand(ButtonPressCommand):
+    def execute(self, ship):
+        ship.rot -= 5
+
+class ButtonPressSpaceCommand(ButtonPressCommand):
+    def execute(self, ship):
+        if abs(ship.speed_y) < 1:
+            ship.speed_y = 0
+        else:
+            ship.speed_y *= 0.95
+        if abs(ship.speed_x) < 1:
+            ship.speed_x = 0
+        else:                                
+            ship.speed_x *= 0.95
 
 class Shot(pygame.sprite.Sprite):
     def __init__(self, ship):
         super().__init__()
         self.speed = ship.speed_limit * 1.2
-        self.image = pygame.transform.rotate(pygame.image.load("images/shot.png"), ship.rot)
-        self.image.set_colorkey((0,0,0))
+        self.image = pygame.transform.rotate(
+            pygame.image.load("images/shot.png"), ship.rot)
+        self.image.set_colorkey((0, 0, 0))
         self.rot = ship.rot % 360
         self.rect = self.image.get_rect()
-        self.rect.center = (ship.rect.center[0] - 40 * sin((self.rot) * pi / 180) - ship.side * 30 * cos((self.rot) * pi / 180), 
+        self.rect.center = (ship.rect.center[0] - 40 * sin((self.rot) * pi / 180) - ship.side * 30 * cos((self.rot) * pi / 180),
                             ship.rect.center[1] - 40 * cos((self.rot) * pi / 180) + ship.side * 30 * sin((self.rot) * pi / 180))
 
     def update(self):
         self.rect.x += self.speed * cos((self.rot % 360) * pi / 180 + pi / 2)
         self.rect.y -= self.speed * sin((self.rot % 360) * pi / 180 + pi / 2)
+
+
+
 
 class Flame(pygame.sprite.Sprite):
     def __init__(self, ship):
@@ -61,44 +112,20 @@ class Ship(pygame.sprite.Sprite):
         self.shot_delay = 0.3
         self.side = 1
         self.speed_limit = 10
+        self.comands = {pygame.K_a : ButtonPressACommand(),
+                        pygame.K_d : ButtonPressDCommand(),
+                        pygame.K_w : ButtonPressWCommand(),
+                        pygame.K_SPACE : ButtonPressSpaceCommand(),
+                        pygame.K_RETURN : ButtonPressReturnCommand(),}
 
     def update(self):
         if not pygame.mixer.Channel(0).get_busy() and self.is_move:
             pygame.mixer.Channel(0).play(pygame.mixer.Sound(r'sounds\thrust.wav'))
         keys=pygame.key.get_pressed()
         self.is_move = False   
-        if keys[pygame.K_a]:  #left
-            self.rot += 5
-        if keys[pygame.K_d]:  #right
-            self.rot -= 5
-        if keys[pygame.K_w]:  #up
-            self.speed_x += cos((self.rot % 360) * pi / 180 + pi / 2) / 10
-            self.speed_y += -1*sin((self.rot % 360) * pi / 180 + pi / 2) / 10
-            if (self.speed_x ** 2 + self.speed_y ** 2) ** 0.5 > self.speed_limit:
-                self.speed_x *= self.speed_limit / (self.speed_x ** 2 + self.speed_y ** 2)**0.5
-                self.speed_y *= self.speed_limit / (self.speed_x ** 2 + self.speed_y ** 2)**0.5
-            self.is_move = True
-        if keys[pygame.K_SPACE]:  #stop
-            if abs(self.speed_y) < 1:
-                self.speed_y = 0
-            else:
-                self.speed_y *= 0.95
-            if abs(self.speed_x) < 1:
-                self.speed_x = 0
-            else:                                
-                self.speed_x *= 0.95
-        if keys[pygame.K_RETURN]:
-            if time() - self.last_shot > self.shot_delay:
-                if not pygame.mixer.Channel(1).get_busy():
-                    pygame.mixer.Channel(1).set_volume(0.05)
-                    pygame.mixer.Channel(1).play(pygame.mixer.Sound(r'sounds\fire.mp3'))
-
-                self.shots.append(Shot(self))
-                self.last_shot = time()
-                if self.side == 1:
-                    self.side = -1
-                elif self.side == -1:
-                    self.side = 1
+        for command in self.comands.keys():
+            if keys[command]:
+                self.comands[command].execute(self)
 
         if self.rect.center[0] > self.width:
             self.rect.center = (0, self.rect.center[1])
